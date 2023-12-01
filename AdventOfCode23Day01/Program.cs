3f8e326576145b75f04275f128004e7352cd4a45
William Sullivan <william@symbotics.co.uk>
2023-12-01 04:41:56 -0500

    Day 1 part 2

--- AdventOfCode23Day01/Program.cs
+++ AdventOfCode23Day01/Program.cs
@@ -1,8 +1,9 @@
 ﻿using AdventOfCode23Day01.Properties;
+using System.Text.RegularExpressions;
 
 string input = Resources.Input1;
 
-List<int> values = [];
+List<int> values1 = [];
 foreach (string line in input.Split(Environment.NewLine))
 {
 	bool firstFound = false;
@@ -20,7 +21,46 @@ foreach (string line in input.Split(Environment.NewLine))
 				last = c - '0';
 		}
 	}
-	values.Add((10 * first) + last);
+	values1.Add((10 * first) + last);
 }
 
-Console.WriteLine($"Sum of calibration values: {values.Sum()}");
+Console.WriteLine($"Sum of calibration values: {values1.Sum()}");
+
+List<int> values2 = [];
+foreach (string line in input.Split(Environment.NewLine))
+{
+	IEnumerable<int> matches = NumberRegex().Matches(line).Cast<Match>().Select(m => StringToInt(m.Groups[1].Value));
+	int first = matches.First();
+	int last = matches.Last();
+
+	values2.Add((10 * first) + last);
+}
+
+Console.WriteLine($"Sum of new calibration values: {values2.Sum()}");
+
+
+
+
+
+internal partial class Program
+{
+	[GeneratedRegex(@"(?=(one|two|three|four|five|six|seven|eight|nine|\d))")]
+	private static partial Regex NumberRegex();
+
+	private static int StringToInt(string input)
+	{
+		return input switch
+		{
+			"one" => 1,
+			"two" => 2,
+			"three" => 3,
+			"four" => 4,
+			"five" => 5,
+			"six" => 6,
+			"seven" => 7,
+			"eight" => 8,
+			"nine" => 9,
+			_ => input[0] - '0'
+		};
+	}
+}
