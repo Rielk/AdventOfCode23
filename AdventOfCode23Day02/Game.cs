cd2e1414940376d821e598c403a1cc8c76412466
William Sullivan <william@symbotics.co.uk>
2023-12-02 03:14:57 -0500

    Day 2 part 1

---
+++ AdventOfCode23Day02/Game.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode23Day02;
+internal class Game
+{
+	public int ID;
+	public List<DrawSet> DrawSets = [];
+
+	public Game(int id, string gameString)
+	{
+		ID = id;
+
+		foreach (string setString in gameString.Split(';'))
+		{
+			int red = 0, green = 0, blue = 0;
+			foreach (string s in setString.Split(','))
+			{
+				if (s.Contains("red"))
+					red += getNumber(s);
+				else if (s.Contains("green"))
+					green += getNumber(s);
+				else if (s.Contains("blue"))
+					blue += getNumber(s);
+
+				static int getNumber(string s) => int.Parse(string.Concat(s.Where(char.IsDigit)));
+			}
+			DrawSets.Add(new(red, green, blue));
+		}
+	}
+
+	public bool CanContentsBe(DrawSet testSet) => DrawSets.All(testSet.CanDraw);
+}
---
+++ AdventOfCode23Day02/Program.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode23Day02;
+using AdventOfCode23Day02.Properties;
+
+string input = Resources.Input1;
+
+List<Game> Games = [];
+foreach (string line in input.Split(Environment.NewLine))
+{
+	string[] split = line.Split(':');
+	int id = int.Parse(string.Concat(split[0].Where(char.IsDigit)));
+	Games.Add(new(id, split[1]));
+}
+
+DrawSet TestSet = new(12, 13, 14);
+
+int sumOfPossible = Games.Where(g => g.CanContentsBe(TestSet)).Select(g => g.ID).Sum();
+
+Console.WriteLine($"Sum of IDs of possible games: {sumOfPossible}");
