f20db66fa40f6a693c90675bcb17b783c50a006b
William Sullivan <william@symbotics.co.uk>
2023-12-06 03:00:59 -0500

    Day 6 part 1

---
+++ AdventOfCode23Day06/Program.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode23Day06;
+using AdventOfCode23Day06.Properties;
+
+string input = Resources.Input1;
+
+string[] split = input.Split(Environment.NewLine);
+IEnumerable<string> times = split[0].Split(' ').Where(x => !string.IsNullOrEmpty(x)).Skip(1);
+IEnumerable<string> distances = split[1].Split(' ').Where(x => !string.IsNullOrEmpty(x)).Skip(1);
+
+var races = times.Zip(distances).Select(x => new Race(int.Parse(x.First), int.Parse(x.Second))).ToList();
+//Race fullRace = new(int.Parse(string.Join("", times)), int.Parse(string.Join("", distances)));
+
+int multiplyWaysToWin = races.Select(r => r.WaysToWin()).Select(ways => ways.Count()).Aggregate((x, y) => x * y);
+
+Console.WriteLine($"Multiplying ways to wind: {multiplyWaysToWin}");
+//Console.WriteLine();
+//Console.WriteLine($"Ways to win full race: {fullRace.WaysToWin().Count()}");
---
+++ AdventOfCode23Day06/Race.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode23Day06;
+internal class Race
+{
+	public int Time { get; }
+	public int Record { get; }
+	public int HoldTimeMax { get; }
+	public int HoldTimeMin { get; }
+
+	public Race(int time, int record)
+	{
+		Time = time;
+		Record = record;
+
+		double sqrt = Math.Sqrt(Math.Pow(Time, 2) - (4 * Record));
+
+		double holdTimeMax = (Time + sqrt) / 2;
+		double holdTimeMin = (Time - sqrt) / 2;
+
+		HoldTimeMax = (int)(holdTimeMax == (int)holdTimeMax ? holdTimeMax - 1 : Math.Floor(holdTimeMax));
+		HoldTimeMin = (int)(holdTimeMin == (int)holdTimeMin ? holdTimeMin + 1 : Math.Ceiling(holdTimeMin));
+	}
+
+	public IEnumerable<int> WaysToWin() => Enumerable.Range(HoldTimeMin, HoldTimeMax - HoldTimeMin + 1);
+}
