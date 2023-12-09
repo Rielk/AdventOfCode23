d7837111e3e782fd2c772c71a8866fae795b5979
William Sullivan <william@symbotics.co.uk>
2023-12-09 02:37:43 -0500

    Day 9 part 1

---
+++ AdventOfCode23Day09/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode23Day09;
+using AdventOfCode23Day09.Properties;
+
+string input = Resources.Input1;
+
+List<RecursiveSeries> series = [];
+foreach (string line in input.Split(Environment.NewLine))
+{
+	IEnumerable<long> split = line.Split(' ').Select(long.Parse).ToList();
+	series.Add(new(split));
+}
+
+long sumOfExtrapolatedValues = series.Select(s => s.GetNValue(s.Length + 1).Last()).Sum();
+
+Console.WriteLine($"Sum of extrapolated values: {sumOfExtrapolatedValues}");
---
+++ AdventOfCode23Day09/RecursiveSeries.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace AdventOfCode23Day09;
+internal class RecursiveSeries : IEnumerable<long>
+{
+	public long Start { get; }
+	public RecursiveSeries? Differences { get; }
+	public int Length { get; }
+
+	public RecursiveSeries(IEnumerable<long> values)
+	{
+		Start = values.First();
+		List<long> diffs = [];
+		long first = values.First();
+		foreach (long second in values.Skip(1))
+		{
+			diffs.Add(second - first);
+			first = second;
+		}
+
+		if (diffs.All(d => d.Equals(0)))
+			Differences = null;
+		else
+			Differences = new(diffs);
+		Length = values.Count();
+	}
+
+	public IEnumerable<long> GetNValue(int n)
+	{
+		if (n == 0) yield break;
+
+		yield return Start;
+		long val = Start;
+		foreach (long dif in Differences?.GetNValue(n - 1) ?? Enumerable.Repeat<long>(0, n - 1))
+		{
+			val += dif;
+			yield return val;
+		}
+	}
+
+	public IEnumerator<long> GetEnumerator() => GetNValue(Length).GetEnumerator();
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
