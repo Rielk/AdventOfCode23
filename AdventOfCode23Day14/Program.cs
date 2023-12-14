56b5e33b77e500536f990f1ff5ded15fd15cfd2c
William Sullivan <william@symbotics.co.uk>
2023-12-14 03:02:01 -0500

    Day 14 part 1

---
+++ AdventOfCode23Day14/Program.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCode23Day14;
+using AdventOfCode23Day14.Properties;
+
+string input = Resources.Input1;
+
+Dish dish = new(input.Split(Environment.NewLine));
+dish.TiltNorth();
+
+int totalLoad = dish.TotalLoad;
+
+Console.WriteLine($"Total Load: {totalLoad}");
---
+++ AdventOfCode23Day14/Rock.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode23Day14;
+internal enum Rock
+{
+	Cube = '#', Round = 'O', None = '.'
+}
+
+internal static class RockExtensions
+{
+	internal static Rock ToRock(this char c)
+	{
+		if (Enum.IsDefined(typeof(Rock), (int)c))
+			return (Rock)c;
+		throw new ArgumentException($"\"{c}\" is not a valid Rock character");
+	}
+}
