9012c7a03f32ec4439f58156b6ec97f89684e201
William Sullivan <william@symbotics.co.uk>
2023-12-15 01:46:20 -0500

    Day 15 part 1

---
+++ AdventOfCode23Day15/HASHAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace AdventOfCode23Day15;
+internal class HASHAlgorithm(string input)
+{
+	public string String { get; } = input;
+
+	private int? hashValue = null;
+	public int HashValue => GetHashCode();
+
+	public override int GetHashCode()
+	{
+		if (hashValue.HasValue) return hashValue.Value;
+
+		hashValue = 0;
+		foreach (byte b in Encoding.ASCII.GetBytes(String))
+		{
+			hashValue += b;
+			hashValue *= 17;
+			hashValue %= 256;
+		}
+		return hashValue.Value;
+	}
+}
---
+++ AdventOfCode23Day15/Program.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode23Day15;
+using AdventOfCode23Day15.Properties;
+
+string input = Resources.Input1;
+
+List<HASHAlgorithm> hashValues = [];
+foreach (string s in input.Split(','))
+	hashValues.Add(new(s));
+
+int hashTotals = hashValues.Select(h => h.HashValue).Sum();
+
+Console.WriteLine($"Sum of HASH values: {hashTotals}");
