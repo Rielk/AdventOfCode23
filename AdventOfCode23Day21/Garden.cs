e73c80452493d5e33c6d7f610598ec58edad3c79
William Sullivan <william@symbotics.co.uk>
2023-12-21 17:41:32 -0500

    Day 21 part 1 optimized

--- AdventOfCode23Day21/Garden.cs
+++ AdventOfCode23Day21/Garden.cs
@@ -1,17 +1,32 @@
 ﻿
 
+using AdventOfCode23Utilities;
+
 namespace AdventOfCode23Day21;
 internal class Garden
 {
 	private Tile[,] Tiles { get; }
+	private bool[,] StandOptions { get; set; }
 
 	public int Width { get; }
 	public int Height { get; }
 
 	public Location Start { get; }
 
-	private List<Location> CurrentLocations { get; set; }
-	public int TotalPossible => CurrentLocations.Count;
+	public int Steps { get; private set; } = 0;
+
+	public int TotalPossible
+	{
+		get
+		{
+			int count = 0;
+			foreach (bool value in StandOptions)
+				if (value)
+					count++;
+			return count;
+
+		}
+	}
 
 	public const char StartChar = 'S';
 	public Garden(string[] input)
@@ -20,6 +35,7 @@ internal class Garden
 		Width = input[0].Length;
 
 		Tiles = new Tile[Width, Height];
+		StandOptions = new bool[Width, Height];
 		foreach ((string s, int y) in input.Select((x, i) => (x, i)))
 			foreach ((char c, int x) in s.Select((x, i) => (x, i)))
 			{
@@ -27,27 +43,35 @@ internal class Garden
 					Start = new(x, y);
 				Tiles[x, y] = c.ToTile();
 			}
-
-		CurrentLocations = [Start];
+		StandOptions[Start.X, Start.Y] = true;
 	}
 
 	internal void TakeSteps(int steps)
 	{
 		foreach (int _ in Enumerable.Range(0, steps))
-		{
-			List<Location> newLocations = [];
-			foreach (Location location in CurrentLocations)
-				foreach (Location adjacentLocation in location.AdjacentLocation())
-					if (GetTile(adjacentLocation) == Tile.Plot && !newLocations.Contains(adjacentLocation))
-						newLocations.Add(adjacentLocation);
-			CurrentLocations = newLocations;
-		}
+			TakeStep();
 	}
 
-	public Tile GetTile(Location location)
+	private void TakeStep()
 	{
-		if (location.X < 0 || location.X >= Width || location.Y < 0 || location.Y >= Height)
-			return Tile.Rock;
-		return Tiles[location.X, location.Y];
+		bool[,] newStandOptions = new bool[Width, Height];
+		foreach (int y in Enumerable.Range(0, Height))
+			foreach (int x in Enumerable.Range(0, Width))
+			{
+				var checkLocation = new Location(x, y);
+				if (!Tiles.TryGetValue(checkLocation.X, checkLocation.Y, out Tile tile))
+					continue;
+				if (tile != Tile.Plot)
+					continue;
+				foreach (Location adjacentLocation in checkLocation.AdjacentLocations())
+					if (StandOptions.TryGetValue(adjacentLocation.X, adjacentLocation.Y, out bool hasAdjacent))
+						if (hasAdjacent)
+						{
+							newStandOptions[x, y] = true;
+							break;
+						}
+			}
+		StandOptions = newStandOptions;
+		Steps++;
 	}
 }
--- AdventOfCode23Day21/Location.cs
+++ AdventOfCode23Day21/Location.cs
@@ -1,7 +1,7 @@
 ﻿namespace AdventOfCode23Day21;
 internal readonly record struct Location(int X, int Y)
 {
-	public IEnumerable<Location> AdjacentLocation()
+	public IEnumerable<Location> AdjacentLocations()
 	{
 		yield return new Location(X - 1, Y);
 		yield return new Location(X + 1, Y);
